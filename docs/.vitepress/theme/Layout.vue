19f8279fc8d74b16358061b9595aac965b1a94ee
Hacxy <hacxy.97@outlook.com>
2024-03-11 12:59:18 +0800

    fix: 修复生产环境oml2d图标加载异常的问题

---
+++ docs/.vitepress/theme/Layout.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { onMounted } from "vue";
+
+import BlogTheme from "@sugarat/theme";
+const Layout = BlogTheme.Layout;
+
+onMounted(async () => {
+  const { loadOml2d } = await import("oh-my-live2d");
+
+  loadOml2d({
+    models: [
+      {
+        path: "https://cdn.jsdelivr.net/gh/Eikanya/Live2d-model/Live2D/Senko_Normals/senko.model3.json",
+      },
+    ],
+  });
+});
+</script>
+
+<template>
+  <!-- <div class="example"></div> -->
+  <Layout />
+</template>
+
+<style scoped lang="scss"></style>
--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,5 +1,5 @@
 import BlogTheme from "@sugarat/theme";
-
+import Layout from "./Layout.vue";
 // 自定义样式重载
 // import './style.scss'
 
@@ -7,16 +7,5 @@ import BlogTheme from "@sugarat/theme";
 // import './user-theme.css'
 export default {
   ...BlogTheme,
-  async enhanceApp() {
-    if (!(import.meta as any).env.SSR) {
-      const { loadOml2d } = await import("oh-my-live2d");
-      loadOml2d({
-        models: [
-          {
-            path: "https://cdn.jsdelivr.net/gh/Eikanya/Live2d-model/Live2D/Senko_Normals/senko.model3.json",
-          },
-        ],
-      });
-    }
-  },
+  Layout,
 };
